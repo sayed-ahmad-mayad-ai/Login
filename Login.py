00e6f57f3925e33a41f762aff7132ae90c18bf1a
sayed-ahmad-mayad-ai <sayed.ahmad.mayad@gmail.com>
2025-12-15 22:03:31 +0200

    Login

--- Login.py
+++ Login.py
@@ -1,12 +1,12 @@
 def GUI(NAME):
     print(f"""
-          ||#################################||
-          ||                                 ||
-          ||        Welcome to the GUI       ||
-          ||        Calculator {NAME}        ||
-          ||                                 ||
-          ||#################################||
-          """)
+      ||#################################||
+      ||                                 ||
+      ||        Welcome to the GUI       ||
+      ||        Calculator {NAME}        ||
+      ||                                 ||
+      ||#################################||
+    """)
 
 
 def check_email():
@@ -15,22 +15,25 @@ def check_email():
         email = input("Enter email: ")
         password = input("Enter password: ")
 
-        if username and email  and password :
-            print("\nLogin successful!")
-            GUI(username)
-            break
+        if not username or not email or not password:
+            print("\nPlease fill in all fields. Try again.\n")
+
         elif not username.isalnum():
             print("\nUsername must be alphanumeric. Try again.\n")
+
         elif len(username) < 3:
             print("\nUsername must be at least 3 characters long. Try again.\n")
+
         elif len(password) < 6:
             print("\nPassword must be at least 6 characters long. Try again.\n")
-        elif "@" not in email or "." not in email:
+
+        elif "@" not in email and "." not in email:
             print("\nInvalid email format. Try again.\n")
-        elif not username or not email or not password:
-            print("\nPlease fill in all fields. Try again.\n")
+
         else:
-            print("\nInvalid credentials. Try again.\n")
+            print("\nLogin successful!\n\n")
+            GUI(username)
+            break
 
 
 check_email()
